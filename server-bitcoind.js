4ae4142ef89ab4bc47bfc0f76cb4fcd2ec613d30
Zhong Zhuang <naituida@foxmail.com>
2013-05-01 18:39:16 +0800

    naive log

--- server-bitcoind.js
+++ server-bitcoind.js
@@ -1,7 +1,8 @@
 var rpc = require('./libs/jsonrpc');
 var server = new rpc.Server();
+var fs=require('fs');
 var argv = require('optimist')
-    .usage('Usage: $0 -o [bitcoind rpc port] -p [mining port]')
+    .usage('Usage: $0 -n [server name]-o [bitcoind rpc port] -p [mining port]')
     .demand(['o','p'])
     .argv;
 
@@ -12,17 +13,37 @@ var kapitalize = require('./libs/kapitalize')({
     port:argv.o
   });
 
+var shares = 0;
+var dropbox_path = "/home/david/Dropbox/";
+
 function getwork(args, opt, callback) {
   if(args.length==0) {
     kapitalize.getwork(function(err,res) {
       callback(null,res);
       });
   } else {
+    shares+=1;
     kapitalize.getwork(args[0],function(err,res) {
+      if(res.result==true) {
+	// LMFAO!!!! We found a block!!!!!
+	fs.appendFile(dropbox_path+'blocks.log',new Date());	
+      }
       callback(null,"trueDannyIsAFuckingAssHoleDannyIsAFuckingAssHoleDannyIsAFuckingAssHoleDannyIsAFuckingAssHole");
     });
   }
 }
+
+
+function update_share() {
+  console.log(shares);
+  var name = argv.n;
+  var cur = (new Date()).getTime();
+  var info = {"name":name,"timestamp":cur,"shares":shares};
+  fs.appendFile(dropbox_path+name+'.log',JSON.stringify(info));
+  shares = 0;
+};
+
+setInterval(update_share, 60*1000);
 
 server.expose('getwork', getwork);
 
