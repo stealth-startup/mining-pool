0566c6e5cbe31bcc2a8f7a6560eaed86f3b932c3
naituida <dyzz@mail.ustc.edu.cn>
2013-08-03 18:49:21 +0800

    merge fix

--- report/merge.js
+++ report/merge.js
@@ -3,14 +3,14 @@ Array.prototype.unique = function(f) {
   for(var i=0; i<a.length; ++i) {
     for(var j=i+1; j<a.length; ++j) {
       if(f(a[i]) === f(a[j]))
-        a.splice(i--, 1);
+        a.splice(j--, 1);
     }
   }
   return a;
 };
 
 function merge(arr1,arr2) {
-  return arr1.concat(arr2).unique(function(item){return item.url;}); 
+  return arr2.concat(arr1).unique(function(item){return item.url;}); 
 }
 
 module.exports = merge;
