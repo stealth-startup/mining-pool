b7cbc9755f80b7c55fd53807cfbff8e2edf2b809
naituida <dyzz@mail.ustc.edu.cn>
2013-08-08 17:20:51 +0800

    get block info from blockchain.info

---
+++ report/blockchain.js
@@ -0,0 +1,28 @@
+var https = require('https');
+
+var root = 'https://blockchain.info';
+var param_json = '?format=json';
+
+exports.getblock = function (hash,callback) {
+    var url=root+'/block-index/'+hash+param_json;
+    https.get(url, function(res) {
+	var body = '';
+	
+	res.on('data', function(chunk) {
+            body += chunk;
+	});
+	
+	res.on('end', function() {
+	    try {
+		var res = JSON.parse(body);
+		callback(res);
+	    } catch(e) {
+		console.log("Failed at: ",url);
+	    }
+	});
+    }).on('error', function(e) {
+	console.log("Got error: ", e);
+    });
+}
+
+//getblock('000000000000003bbba777203b480e169ff4da727079c3c78d5e6274a826b664');
