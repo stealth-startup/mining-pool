54f1d6c2a09f5cf2ec9205e7c8b2dba1c32e7f6e
naituida <dyzz@mail.ustc.edu.cn>
2013-08-22 08:59:22 +0800

    detach erpao to new repository

--- report/test.js
+++
@@ -1,9 +0,0 @@
-var mongoDbConnection = require('./connection');
-
-mongoDbConnection(function(databaseConnection) {
-    databaseConnection.collection('blocks')
-                      .find({'time':{$gte:1375690741000}})
-                      .toArray(function(err,arr){console.log(arr);});
-});
-
-
