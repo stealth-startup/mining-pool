932a6aa75c2a678018fc952a261b9fd97dee4313
Zhong Zhuang <naituida@foxmail.com>
2013-09-05 14:35:55 +0800

    gen_config fix;TODO actually generate script

--- script/gen_config.py
+++ script/gen_config.py
@@ -3,8 +3,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Generate Config Scripts',epilog='e.g. : python gen_config.py -t 100 101 102 103 -r 100 191')
 
-parser.add_argument("-t", "--tank",  nargs="+",help="tank",required=True,action="store")
-parser.add_argument("-r", "--range", nargs="+",help="ip",required=True,action="store")
+parser.add_argument("-t", "--tank",  nargs="+",help="tank",type=int,required=True,action="store")
+parser.add_argument("-r", "--range", nargs=2,help="ip",type=int,required=True,action="store")
 
 if len(sys.argv)==1:
     parser.print_help()
