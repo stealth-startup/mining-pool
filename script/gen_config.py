511940f9d9f147268f3db3cfba85b49fa8df7cf3
naituida <dyzz@mail.ustc.edu.cn>
2013-09-15 19:53:25 +0800

    gen tank config

--- script/gen_config.py
+++ script/gen_config.py
@@ -5,8 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Generate Config Scripts',epilog='python gen_config.py -t 100 101 102 103 -r 100 191 -u 192.168.0.5 -p 8334')
 
-parser.add_argument("-t", "--tank",  nargs="+",help="tank",type=int,required=True,action="store")
-parser.add_argument("-r", "--range", nargs=2,help="ip",type=int,required=True,action="store")
+parser.add_argument("-t", "--tank", help="tank",type=int,required=True,action="store")
 parser.add_argument("-u", "--url", help="mining url",required=True,action="store")
 parser.add_argument("-p", "--port", help="port",required=True,action="store")
 
@@ -16,22 +15,21 @@ if len(sys.argv)==1:
 
 args = parser.parse_args()
 
-tanks = args.tank
-start = args.range[0]
-end = args.range[1]
+tank = args.tank
+start = 100
+end = 191
 url = args.url
 port = args.port
 
-for tank in tanks:
-    fname= "tank-%s.py" % tank
-    print "Generating "+fname
-    with open(fname,"w") as output:
-        os.chmod(fname, 0744)
-        content="#!/bin/sh\n./config_auto.py -r %d -s %d -e %d -u %s -p %s -w tank%d\n" % (tank,start,end,url,port,tank)
-        print content
-        output.write(content)
-        output.close()
-
+fname= "tank-%s.py" % tank
+print "Generating "+fname
+with open(fname,"w") as output:
+    os.chmod(fname, 0744)
+    content="#!/bin/sh\n./config_auto.py -r %d -s %d -e %d -u %s -p %s -w tank%d\n" % (tank,start,end,url,port,tank)
+    print content
+    output.write(content)
+    output.close()
+    
 
 
 
